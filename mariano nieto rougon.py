fabae06c9e74d823b14c50a9ac8bba5c508905b8
marianonietor <mnietor@colegioalamos.edu.mx>
2021-10-18 20:05:18 -0500

    acabe las preguntas

---
+++ mariano nieto rougon.py
@@ -0,0 +1,33 @@
+mariano nieto rougon
+mauricio martinez magaña
+
+
+en tus palabras ¿Que es internet?
+para mi internet en pocas palabras es una forma de conectar y compartir informacion de varias computadoras.
+
+describe los medios fisicos y de propagacion de ondas por los que se transmite la informacion en internet.
+internet funciona por una coneccion de redes y reglas entre las computadoras
+
+¿Como se organiza internet? Habla de las direcciones IP y los direcciones
+todos los dispositivos en internet tienen una direccion que es la IP para organizar mensajes y cosas que llegan ahi
+es como si la informacion fuera mandada a solo ese numero y esa computadora lo recibiera
+
+describe el procedimiento por el cual una cancion de spotify llega al servidor de la compañia a tu celular
+no es de forma directa, tiene que pasar de un lado a otro para llegar
+asi llega mas rapido atravesando los caminos que sean mas libres dependiendo de que tan saturados esten
+
+Describe los protocolos que existen en internet, en especifico https y html
+cuando escribes https para dirigirte a una pagina es cuando tu computadora se conecta con otra mediante internet
+y html es un lenguaje de programacion el cual transforma en imagen que aparece en la computadora gracias al codigo
+
+en dos parrafos cuentanos lo que aprendiste sobre encriptacion y llaves publicas
+encriptar es una forma de achicar un codigo para que solo otra computaora pueda desifrarlo
+como si estuvieras mandando un mensaje con llave, que solo la otra computadora podra desifrar y abrir
+
+en dos parrafos cuentanos lo que aprendiste sobre como protegerte de los criminales en internet
+para cuidarte de hackers y criminales debes de cuidar tus contraseñas y informacion de la forma mas segura posible
+actualizar tus softwares tambien ayuda a que haya menor probabilidad de recibir un virus
+
+como funciona la busqueda en google?
+cuando buscas algo tu computadora se une a una red que tiene toda la informacion que ha sido guardada con el tiempo 
+
